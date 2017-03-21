6e7ea6fb947f915b7305da17f33d026bdc61d61b
cmibarnwell <calebbarnwell@gmail.com>
2017-03-21 12:32:15 -0500

    Initialized the Driver, Header, and Makefile for Program5

---
+++ Program5/cs2123p5.h
@@ -0,0 +1,74 @@
+/**********************************************************************
+cs2123p5.h by Caleb Barnwell, Brandon Lwowski, and Brandon Bell
+Purpose:
+    Defines constants:
+        max constants
+        error constants
+        boolean constants
+
+    Defines typedef for
+        For Linked List
+            NodeLL
+            LinkedListImp
+            LinkedList
+        For Graph
+            Vertex
+            GraphImp
+            Graph
+
+ Protypes
+        
+Notes:
+
+**********************************************************************/
+
+/*** constants ***/
+// Maximum constants
+#define LINE_SIZE 50 //May need to change this value! -Caleb
+
+// Error constants (program exit values)
+#define ERR_COMMAND_LINE    900    // invalid command line argument
+#define ERR_ALGORITHM       903    // Error in algorithm - almost anything else
+#define ERR_BAD_INPUT       503    // Bad input
+
+// boolean constants
+#define FALSE 0
+#define TRUE 1
+
+// exitUsage control 
+#define USAGE_ONLY          0      // user only requested usage information
+#define USAGE_ERR           -1     // usage error, show message and usage information
+
+// boolean constants
+#define FALSE 0
+#define TRUE 1
+
+/* EOF */
+#define REACHED_EOF 1
+
+/*** typedef ***/
+// typedefs for the Linked Lists, THE BELOW WILL BE NEEDING UPDATING AND MAY BE UNNECESSARY, I WILL FIX THIS SOON -Caleb
+typedef struct
+{
+    //SOME DATA, FILL THIS SPOT LATER!!! -Caleb
+    struct Node *pNext;
+} NodeLL;
+typedef struct
+{
+    NodeLL *pHead;
+} LinkedListImp;
+
+typedef LinkedListImp * LinkedList;
+
+// typedefs for the Graphs
+typedef struct
+{
+    int iVertex
+    struct * EdgeNode *pNextEdge;
+} EdgeNode;
+typedef struct
+{
+    EdgeNode * predecessorList;
+    char szLabel[LABEL_SIZE+1];
+    EdgeNode * successorList;
+} Vertex;
---
+++ Program5/cs2123p5Driver.c
@@ -0,0 +1,35 @@
+/****************************************************************
+ cs2123p5Driver.c by Caleb Barnwell
+ 
+ Purpose:
+     The driver file for Program 5
+ Input:
+    
+
+ Returns:
+     
+ Notes:
+     
+ ****************************************************************/
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include "cs2123p5.h"
+
+ int main(int argc, char *argv[])
+ {
+
+ }
+
+/**************************
+Start Graph Funcs
+**************************/
+
+
+
+/**************************
+End Graph Funcs
+**************************/
