340509a1932d9b6e031b57a7dc838c8866168ecf
cmibarnwell <calebbarnwell@gmail.com>
2017-03-28 10:49:04 -0500

    Added exiterr and exitusage to driver

--- Program5/cs2123p5Driver.c
+++ Program5/cs2123p5Driver.c
@@ -80,3 +80,94 @@ char * getToken(char *pszInputTxt, char szToken[], int iTokenSize)
     else
         return pszInputTxt + 1;
 }
+
+/******************** ErrExit **************************************
+  void ErrExit(int iexitRC, char szFmt[], ... )
+Purpose:
+    Prints an error message defined by the printf-like szFmt and the
+    corresponding arguments to that function.  The number of
+    arguments after szFmt varies dependent on the format codes in
+    szFmt.
+    It also exits the program with the specified exit return code.
+Parameters:
+    I   int iexitRC             Exit return code for the program
+    I   char szFmt[]            This contains the message to be printed
+                                and format codes (just like printf) for
+                                values that we want to print.
+    I   ...                     A variable-number of additional arguments
+                                which correspond to what is needed
+                                by the format codes in szFmt.
+Returns:
+    Returns a program exit return code:  the value of iexitRC.
+Notes:
+    - Prints "ERROR: " followed by the formatted error message specified
+      in szFmt.
+    - Prints the file path and file name of the program having the error.
+      This is the file that contains this routine.
+    - Requires including <stdarg.h>
+**************************************************************************/
+void ErrExit(int iexitRC, char szFmt[], ... )
+{
+    va_list args;               // This is the standard C variable argument list type
+    va_start(args, szFmt);      // This tells the compiler where the variable arguments
+    // begins.  They begin after szFmt.
+    printf("ERROR: ");
+    vprintf(szFmt, args);       // vprintf receives a printf format string and  a
+    // va_list argument
+    va_end(args);               // let the C environment know we are finished with the
+    // va_list argument
+    printf("\n\tEncountered in file %s\n", __FILE__);  // this 2nd arg is filled in by
+    // the pre-compiler
+    exit(iexitRC);
+}
+/******************** exitUsage *****************************
+    void exitUsage(int iArg, char *pszMessage, char *pszDiagnosticInfo)
+Purpose:
+    In general, this routine optionally prints error messages and diagnostics.
+    It also prints usage information.
+
+    If this is an argument error (iArg >= 0), it prints a formatted message
+    showing which argument was in error, the specified message, and
+    supplemental diagnostic information.  It also shows the usage. It exits
+    with ERR_COMMAND_LINE.
+
+    If this is a usage error (but not specific to the argument), it prints
+    the specific message and its supplemental diagnostic information.  It
+    also shows the usage and exist with ERR_COMMAND_LINE.
+
+    If this is just asking for usage (iArg will be -1), the usage is shown.
+    It exits with USAGE_ONLY.
+Parameters:
+    I int iArg                      command argument subscript or control:
+                                    > 0 - command argument subscript
+                                    0 - USAGE_ONLY - show usage only
+                                    -1 - USAGE_ERR - show message and usage
+    I char *pszMessage              error message to print
+    I char *pszDiagnosticInfo       supplemental diagnostic information
+Notes:
+    This routine causes the program to exit.
+**************************************************************************/
+void exitUsage(int iArg, char *pszMessage, char *pszDiagnosticInfo)
+{
+    switch (iArg)
+    {
+        case USAGE_ERR:
+            fprintf(stderr, "Error: %s %s\n"
+                    , pszMessage
+                    , pszDiagnosticInfo);
+            break;
+        case USAGE_ONLY:
+            break;
+        default:
+            fprintf(stderr, "Error: bad argument #%d.  %s %s\n"
+                    , iArg
+                    , pszMessage
+                    , pszDiagnosticInfo);
+    }
+    // print the usage information for any type of command line error
+    fprintf(stderr, "p2 -c courseFileName -q queryFileName\n");
+    if (iArg == USAGE_ONLY)
+        exit(USAGE_ONLY);
+    else
+        exit(ERR_COMMAND_LINE);
+}
