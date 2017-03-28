b2e981456dfe3684f69f525f98a569a15395aa2b
cmibarnwell <calebbarnwell@gmail.com>
2017-03-28 09:35:48 -0500

    Fixed up makefile and added CMBp5.c

---
+++ Program5/CMBp5.c
@@ -0,0 +1,21 @@
+/****************************************************************
+ CMBp5.c by Caleb Barnwell
+
+ Purpose:
+
+ Input:
+
+
+ Returns:
+
+ Notes:
+
+ ****************************************************************/
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include "cs2123p5.h"
+
