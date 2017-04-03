66f9995299356ee632fc4014cd9a1190a818ec5c
Brandon Bell <accelerator@Brandons-MacBook-Pro.local>
2017-04-03 16:23:37 -0500

    Finished causeCycle, may need to be improved or changed later.

--- Program5/BKBp5.c
+++ Program5/BKBp5.c
@@ -4,7 +4,28 @@
 
 int causeCycle(Graph graph, int iPrereqVertex, int iVertex)
 {
-	
+  //iVertex is the subscript needed to get the correct vertex
+  //within the vertex array.
+  Vertex v = graph->vertexM[iVertex];
+
+  //this part is a bit contested, I either need the prereq list or 
+  //the successor list.
+  //Judging by the paper clark gave us, I'm assuming that its the successor
+  //list.
+  EdgeNode* e = v.successorList;
+  //since this adjacency list is represented with a 
+  //linked list, we can just iterate through the nodes.
+  while(e)
+  {
+    //if the prereq vertex of the successor list
+    //equals the prereq vertex given, then it woulc cause a cycle.
+    if(e->iPrereqVertex == iPrereqVertex)
+    {
+      return TRUE;
+    }
+  }
+
+  return FALSE;
 }
 
 int findCourse(Graph graph, char szCourseId[])
