2a9701177de2515d0e4fb8d3b441efd89ea0590e
blwowski <blwowski@gmail.com>
2017-04-06 10:42:13 -0500

    Update InsertCourse

--- Program5/BJLp5.c
+++ Program5/BJLp5.c
@@ -12,36 +12,39 @@ int insertCourse(Graph graph, char szCourseId[], char szCourseName[])
 	int iFind;
 	int i;
 	iFind = findCourse(graph, szCourseId);
-		//Check if course already exist
-		if(iFind != -1)
-		{
-			printf("Course Already Exist %s\n", szCourseId);
-            return -1;
-		}
-		
-		//Store the Course Id, Name, and Department
-		//strcpy(szCourseId,tempVertex.szCourseId);
-		//strcpy(szCourseName,tempVertex.szCourseName);
-		
-		char szDept[4];
-        int j;
-
-		for(j = 0; j < strlen(szCourseId)-1; j++) {
-            if (isalpha(szCourseId[j])) {
-                szDept[j] = szCourseId[j];
-            } else {
-                break;
-            }
-        }
-
-        strcpy(tempVertex.szDept, szDept);
-		
-        graph->vertexM[graph->iNumVertices] = tempVertex;
+	//Copy SzDept into Vertex		
+	char szDept[4];
+    int j;
+    //Store the letters of szDept into temp variable
+	for(j = 0; j < strlen(szCourseId)-1; j++)
+	{
+		if (isalpha(szCourseId[j])) 
+			szDept[j] = szCourseId[j]; 
+        else 
+			break;
+            
+     }
+	 //copy szDept into the vertez
+     strcpy(tempVertex.szDept, szDept);
+      
+     //Create Vertex in vertexM[]
+     if(iFind == -1)
+     {
+		graph->vertexM[graph->iNumVertices] = tempVertex;
         ++graph->iNumVertices;
-
-    return 0;
-
+	 }
+	 //Course already exist so update szCourseName
+	 else
+	 {
+		 strcpy(graph->vertexM[iFind].szCourseName, tempVertex.szCourseName);
+	 }
+	 return 0;
 }
+	
+	
+	
+	
+	
 
 
 
